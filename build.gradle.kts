65cf1065e9ee3ec8197a66358ba70d6157855700
ZuYun <jonas.jzy@gmail.com>
2026-05-26 21:54:20 +0800

    build(deps): 🚀 update Gradle to 9.4.1 and VCL to 2026.05.26

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,5 @@ plugins {
     alias(vcl.plugins.kotlin.serialization) apply false
     alias(vcl.plugins.hilt) apply false
     alias(vcl.plugins.ksp) apply false
-    alias(vcl.plugins.kotlin.android) apply false
     alias(vcl.plugins.compose.compiler) apply false
 }
